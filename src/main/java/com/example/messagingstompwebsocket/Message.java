62420585c9dd09a0c3d7902435b83056899c57dd
Erick Beloti <erick.beloti@gmail.com>
2020-03-18 23:36:34 -0300

    First Commit

---
+++ src/main/java/com/example/messagingstompwebsocket/Message.java
@@ -0,0 +1,18 @@
+package com.example.messagingstompwebsocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+	private String from;
+	private String text;
+
+}
+
+
+
